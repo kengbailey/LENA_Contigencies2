0c439de513d68f0e0e297caee0bad2cc6d5e877c
Ken Bailey <kgbailey92@gmail.com>
2017-10-07 23:10:44 -0500

    Significant change to app layout made. Added most top frame widgets. Added a file menu

--- src/LenaUI.py
+++ src/LenaUI.py
@@ -23,36 +23,57 @@ class LenaUI:
     def __init__(self, root):
         "UI started on init of class"        
         self.root = root
-        root.title("LENA Contingencies")
-        root.geometry('{}x{}'.format(500, 700))
         root.resizable(False, False)
+        root.title("LENA Contingencies")
+
+        # Create main frames
+        main_frame = ttk.Frame(root)
+        top_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=150)
+        mid_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=300)
+        btm_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=100)
+
+        # create menu
+        menubar = Menu(root) # create menu bar
+        root.config(menu=menubar) # attach menubar to root window
+        file_menu = Menu(menubar) # create "File" menu item 
+        file_menu.add_command(label="Exit", command=self.testing123) # add a command to "File" menu item
+        menubar.add_cascade(label="File", menu=file_menu)   # attach "File" menu item to menubar
+
+        # setup main frames to grid
+        main_frame.grid(row=0, column=0)
+        top_frame.grid(row=0, column=0, sticky=W+E+S+N)
+        mid_frame.grid(row=1, column=0, sticky=W+E+S+N)
+        btm_frame.grid(row=2, column=0, sticky=W+E+S+N)
+
+        # TOP FRAME CONFIG
+        # Create top frame widgets
+        top_dir_label = ttk.Label(top_frame, text="Specify Directory")
+        top_reset_btn = ttk.Button(top_frame, text="RESET", command=self.testing123)
+        top_load_btn = ttk.Button(top_frame, text="LOAD", command=self.testing123)
+        top_input_label = ttk.Label(top_frame, text="Input:")
+        top_output_label = ttk.Label(top_frame, text="Output:")
+        top_format_label = ttk.Label(top_frame, text="Output Format")        
+        top_csv_btn = ttk.Checkbutton(top_frame, text=".csv")
+        top_txt_btn = ttk.Checkbutton(top_frame, text=".txt")
+        top_xl_btn = ttk.Checkbutton(top_frame, text=".xlsx")
+        top_filler = ttk.Label(top_frame, text="      ")
         
-        # create all of the main containers
-        top_frame = Frame(root, bg='white', width=500, height=295)
-        sep_frame1 = Frame(root, bg='grey', width=500, height=5)
-        mid_frame = Frame(root, bg='white', width=500, height=295)
-        sep_frame2 = Frame(root, bg='grey', width=500, height=5)
-        btm_frame = Frame(root, bg='white', width=500, height=100)
 
-        # layout main containers
-        root.grid_rowconfigure(1, weight=1)
-        root.grid_columnconfigure(0, weight=1)
-
-        top_frame.grid(row=0, sticky="ew")
-        sep_frame1.grid(row=1, sticky="ew")
-        mid_frame.grid(row=2, sticky="nsew")
-        sep_frame2.grid(row=3, sticky="ew")
-        btm_frame.grid(row=4, sticky="ew")
-
-        """ TOP FRAME """
-        # create widgets for top_frame
-        tf_title_label = Label(top_frame, text="Specify Directory")
-        tf_in_label = Label(top_frame, text="Input: ")
-
-        # layout widgets in top_frame
-        tf_title_label.grid(row=0, columnspan=1)
-        tf_in_label.grid(row=1, columnspan=1)
+        # setup top frame widgets
+        top_reset_btn.grid(row=0, column=4, sticky=E)
+        top_dir_label.grid(row=1, column=0, columnspan=2)
+        top_input_label.grid(row=2, column=0)
+        top_output_label.grid(row=3, column=0)
+        top_format_label.grid(row=5, column=0, columnspan=2)
+        top_filler.grid(row=4, column=0)
+        top_csv_btn.grid(row=6, column=0)
+        top_txt_btn.grid(row=6, column=1)
+        top_xl_btn.grid(row=6, column=2)
+        top_load_btn.grid(row=0, column=3)
 
         # OSX ONLY - bring window to front
         if platform.system() == MAC:
             os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
+
+    def testing123(event):
+        print("testing 123...")
