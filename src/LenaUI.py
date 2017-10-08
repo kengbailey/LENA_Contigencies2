bb3e1994f2855b9872f831e0b18126a5bed11363
Ken Bailey <kgbailey92@gmail.com>
2017-10-08 09:50:43 -0500

    Added several widgets for the middle frame, halfway done with mid_frame

--- src/LenaUI.py
+++ src/LenaUI.py
@@ -47,21 +47,23 @@ class LenaUI:
 
         # TOP FRAME CONFIG
         # Create top frame widgets
+        csv_var = StringVar()
+        txt_var = BooleanVar()
+        xl_var = BooleanVar()
         top_dir_label = ttk.Label(top_frame, text="Specify Directory")
         top_reset_btn = ttk.Button(top_frame, text="RESET", command=self.testing123)
         top_load_btn = ttk.Button(top_frame, text="LOAD", command=self.testing123)
         top_input_label = ttk.Label(top_frame, text="Input:")
         top_output_label = ttk.Label(top_frame, text="Output:")
         top_format_label = ttk.Label(top_frame, text="Output Format")        
-        top_csv_btn = ttk.Checkbutton(top_frame, text=".csv")
+        top_csv_btn = ttk.Checkbutton(top_frame, text='.csv', command=self.testing123, variable=csv_var,onvalue='this', offvalue='that')
         top_txt_btn = ttk.Checkbutton(top_frame, text=".txt")
         top_xl_btn = ttk.Checkbutton(top_frame, text=".xlsx")
         top_filler = ttk.Label(top_frame, text="      ")
         
-
         # setup top frame widgets
         top_reset_btn.grid(row=0, column=4, sticky=E)
-        top_dir_label.grid(row=1, column=0, columnspan=2)
+        top_dir_label.grid(row=1, column=0, columnspan=2, sticky=N)
         top_input_label.grid(row=2, column=0)
         top_output_label.grid(row=3, column=0)
         top_format_label.grid(row=5, column=0, columnspan=2)
@@ -70,6 +72,41 @@ class LenaUI:
         top_txt_btn.grid(row=6, column=1)
         top_xl_btn.grid(row=6, column=2)
         top_load_btn.grid(row=0, column=3)
+
+        # MID FRAME CONFIG
+        # create mid frame widgets
+        type_var = StringVar()
+        ab_a_var = StringVar()
+        ab_b_var = StringVar()
+        abc_a_var = StringVar()
+        abc_b_var = StringVar()
+        abc_c_var = StringVar()
+        mid_type_label = ttk.Label(mid_frame, text=' Type of Analysis')       
+        mid_ab_btn = ttk.Radiobutton(mid_frame, text='A ---> B', variable=type_var, value='ab')
+        mid_abc_btn = ttk.Radiobutton(mid_frame, text='( A ---> B ) ---> C', variable=type_var, value='abc')        
+        mid_filler_label = ttk.Label(mid_frame, text="     ")
+        mid_conf_label = ttk.Label(mid_frame, text="Configure Analysis")
+        mid_ab_a_btn = ttk.Combobox(mid_frame, textvariable=ab_a_var, width=3)
+        mid_ab_b_btn = ttk.Combobox(mid_frame, textvariable=ab_b_var, width=3)
+        mid_abc_a_btn = ttk.Combobox(mid_frame, textvariable=abc_a_var, width=3)
+        mid_abc_b_btn = ttk.Combobox(mid_frame, textvariable=abc_b_var, width=3)
+        mid_abc_c_btn = ttk.Combobox(mid_frame, textvariable=abc_c_var, width=3)
+        mid_filler_label2 = ttk.Label(mid_frame, text="-----")
+        mid_filler_label3 = ttk.Label(mid_frame, text="      ")
+
+        # setup mid frame widgets
+        mid_type_label.grid(row=0, column=0, columnspan=3)
+        mid_ab_btn.grid(row=1, column=0, columnspan=2)
+        mid_abc_btn.grid(row=2, column=0, columnspan=3)
+        mid_filler_label.grid(row=3, column=0, columnspan=3)
+        mid_conf_label.grid(row=4, column=0, columnspan=3)
+        mid_ab_a_btn.grid(row=5, column=0)
+        mid_ab_b_btn.grid(row=5, column=1)
+        mid_abc_a_btn.grid(row=5, column=3)
+        mid_abc_b_btn.grid(row=5, column=4)
+        mid_filler_label2.grid(row=5, column=2)
+        mid_abc_c_btn.grid(row=5, column=5)
+        mid_filler_label3.grid(row=6, column=0, columnspan=3)
 
         # OSX ONLY - bring window to front
         if platform.system() == MAC:
