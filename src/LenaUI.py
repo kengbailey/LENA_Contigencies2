64e661737a3b97686082a34403e493d3c410b38b
Ken Bailey <kgbailey92@gmail.com>
2017-10-09 21:39:04 -0500

    implemented functions for setting up the individua frames. Much cleaner

--- src/LenaUI.py
+++ src/LenaUI.py
@@ -28,9 +28,9 @@ class LenaUI:
 
         # Create main frames
         main_frame = ttk.Frame(root) # top, mid, btm frames embedded within this frame
-        top_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=150)
-        mid_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=300)
-        btm_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=100)
+        self.top_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=150)
+        self.mid_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=300)
+        self.btm_frame = ttk.Frame(main_frame, borderwidth=5, relief="sunken", width=300, height=100)
 
         # create menu
         menubar = Menu(root) # create menu bar
@@ -43,25 +43,39 @@ class LenaUI:
         # top, mid, btm frames laid out inside main_frame
         # sticky tags used to keep UI elements together when stretched
         main_frame.grid(row=0, column=0) 
-        top_frame.grid(row=0, column=0, sticky=W+E+S+N)
-        mid_frame.grid(row=1, column=0, sticky=W+E+S+N)
-        btm_frame.grid(row=2, column=0, sticky=W+E+S+N)
+        self.top_frame.grid(row=0, column=0, sticky=W+E+S+N)
+        self.mid_frame.grid(row=1, column=0, sticky=W+E+S+N)
+        self.btm_frame.grid(row=2, column=0, sticky=W+E+S+N)
 
+        # Setup Individual Frames
+        self.setup_top_frame()
+        self.setup_mid_frame()
+        self.setup_btm_frame()
+
+        # OSX ONLY - bring window to front
+        if platform.system() == MAC:
+            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
+
+    def testing123(event):
+        print("testing 123...")
+        print(event)
+
+    def setup_top_frame(self):
         # TOP FRAME CONFIG
         # Create top frame widgets
         csv_var = BooleanVar() # holds user selection for csv output
         txt_var = BooleanVar() # holds user selection for txt output
         xl_var = BooleanVar()  # holds user selection for xlsx output
-        top_dir_label = ttk.Label(top_frame, text="Specify Directory")
-        top_reset_btn = ttk.Button(top_frame, text="RESET", command=self.testing123)
-        top_load_btn = ttk.Button(top_frame, text="LOAD", command=self.testing123)
-        top_input_label = ttk.Label(top_frame, text="Input:")
-        top_output_label = ttk.Label(top_frame, text="Output:")
-        top_format_label = ttk.Label(top_frame, text="Output Format")        
-        top_csv_btn = ttk.Checkbutton(top_frame, text='.csv', command=self.testing123, variable=csv_var,onvalue=1, offvalue=0)
-        top_txt_btn = ttk.Checkbutton(top_frame, text=".txt", command=self.testing123, variable=txt_var,onvalue=1, offvalue=0)
-        top_xl_btn = ttk.Checkbutton(top_frame, text=".xlsx", command=self.testing123, variable=xl_var,onvalue=1, offvalue=0)
-        top_filler = ttk.Label(top_frame, text="      ")
+        top_dir_label = ttk.Label(self.top_frame, text="Specify Directory")
+        top_reset_btn = ttk.Button(self.top_frame, text="RESET", command=self.testing123)
+        top_load_btn = ttk.Button(self.top_frame, text="LOAD", command=self.testing123)
+        top_input_label = ttk.Label(self.top_frame, text="Input:")
+        top_output_label = ttk.Label(self.top_frame, text="Output:")
+        top_format_label = ttk.Label(self.top_frame, text="Output Format")        
+        top_csv_btn = ttk.Checkbutton(self.top_frame, text='.csv', command=self.testing123, variable=csv_var,onvalue=1, offvalue=0)
+        top_txt_btn = ttk.Checkbutton(self.top_frame, text=".txt", command=self.testing123, variable=txt_var,onvalue=1, offvalue=0)
+        top_xl_btn = ttk.Checkbutton(self.top_frame, text=".xlsx", command=self.testing123, variable=xl_var,onvalue=1, offvalue=0)
+        top_filler = ttk.Label(self.top_frame, text="      ")
         
         # setup top frame widgets
         top_reset_btn.grid(row=0, column=4, sticky=E)
@@ -75,6 +89,7 @@ class LenaUI:
         top_xl_btn.grid(row=6, column=2)
         top_load_btn.grid(row=0, column=3)
 
+    def setup_mid_frame(self):
         # MID FRAME CONFIG
         # create mid frame widgets
         type_var = StringVar()
@@ -84,25 +99,25 @@ class LenaUI:
         abc_b_var = StringVar()
         abc_c_var = StringVar()
         pause_var = BooleanVar()
-        mid_type_label = ttk.Label(mid_frame, text='Type of Analysis')       
-        mid_ab_btn = ttk.Radiobutton(mid_frame, text='A ---> B', variable=type_var, value='ab')
-        mid_abc_btn = ttk.Radiobutton(mid_frame, text='( A ---> B ) ---> C', variable=type_var, value='abc')        
-        mid_filler_label = ttk.Label(mid_frame, text="     ")
-        mid_conf_label = ttk.Label(mid_frame, text="Configure Analysis")
-        mid_ab_a_btn = ttk.Combobox(mid_frame, textvariable=ab_a_var, width=3)
+        mid_type_label = ttk.Label(self.mid_frame, text='Type of Analysis')       
+        mid_ab_btn = ttk.Radiobutton(self.mid_frame, text='A ---> B', variable=type_var, value='ab')
+        mid_abc_btn = ttk.Radiobutton(self.mid_frame, text='( A ---> B ) ---> C', variable=type_var, value='abc')        
+        mid_filler_label = ttk.Label(self.mid_frame, text="     ")
+        mid_conf_label = ttk.Label(self.mid_frame, text="Configure Analysis")
+        mid_ab_a_btn = ttk.Combobox(self.mid_frame, textvariable=ab_a_var, width=3)
         mid_ab_a_btn['values'] = ('NAN', 'SIL')
-        mid_ab_b_btn = ttk.Combobox(mid_frame, textvariable=ab_b_var, width=3)
-        mid_abc_a_btn = ttk.Combobox(mid_frame, textvariable=abc_a_var, width=3)
-        mid_abc_b_btn = ttk.Combobox(mid_frame, textvariable=abc_b_var, width=3)
-        mid_abc_c_btn = ttk.Combobox(mid_frame, textvariable=abc_c_var, width=3)
-        mid_filler_label2 = ttk.Label(mid_frame, text="-----")
-        mid_pause_label = ttk.Label(mid_frame, text="Pause Duration")
-        mid_filler_label3 = ttk.Label(mid_frame, text="     ")
-        mid_pause_slider = ttk.Scale(mid_frame, orient=HORIZONTAL, length=100, from_=1.0, to=50.0)
-        mid_pause_dn_btn = ttk.Button(mid_frame, text="<", command=self.testing123, width=2)
-        mid_pause_up_btn = ttk.Button(mid_frame, text=">", command=self.testing123, width=2)
-        mid_pause_entry = ttk.Entry(mid_frame, width=3)
-        mid_pause_checkbox = ttk.Checkbutton(mid_frame, text="Enable rounding", command=self.testing123)
+        mid_ab_b_btn = ttk.Combobox(self.mid_frame, textvariable=ab_b_var, width=3)
+        mid_abc_a_btn = ttk.Combobox(self.mid_frame, textvariable=abc_a_var, width=3)
+        mid_abc_b_btn = ttk.Combobox(self.mid_frame, textvariable=abc_b_var, width=3)
+        mid_abc_c_btn = ttk.Combobox(self.mid_frame, textvariable=abc_c_var, width=3)
+        mid_filler_label2 = ttk.Label(self.mid_frame, text="-----")
+        mid_pause_label = ttk.Label(self.mid_frame, text="Pause Duration")
+        mid_filler_label3 = ttk.Label(self.mid_frame, text="     ")
+        mid_pause_slider = ttk.Scale(self.mid_frame, orient=HORIZONTAL, length=100, from_=1.0, to=50.0)
+        mid_pause_dn_btn = ttk.Button(self.mid_frame, text="<", command=self.testing123, width=2)
+        mid_pause_up_btn = ttk.Button(self.mid_frame, text=">", command=self.testing123, width=2)
+        mid_pause_entry = ttk.Entry(self.mid_frame, width=3)
+        mid_pause_checkbox = ttk.Checkbutton(self.mid_frame, text="Enable rounding", command=self.testing123)
         
         # setup mid frame widgets
         mid_type_label.grid(row=0, column=1, columnspan=3)
@@ -124,21 +139,14 @@ class LenaUI:
         mid_pause_entry.grid(row=8, column=0)
         mid_pause_checkbox.grid(row=9, column=1, columnspan=3)
 
+    def setup_btm_frame(self):
         # BOTTOM FRAME CONFIG
         # create bottom frame widgets
-        btm_submit_btn = ttk.Button(btm_frame, text="Submit", command=self.testing123)
-        btm_progress_bar = ttk.Progressbar(btm_frame, orient=HORIZONTAL, length=200, mode='determinate')
-        btm_text_window = Text(btm_frame, width=50, height=3)
+        btm_submit_btn = ttk.Button(self.btm_frame, text="Submit", command=self.testing123)
+        btm_progress_bar = ttk.Progressbar(self.btm_frame, orient=HORIZONTAL, length=200, mode='determinate')
+        btm_text_window = Text(self.btm_frame, width=50, height=3)
 
         # arrange bottom frame widgets
         btm_submit_btn.grid(row=0, column=0)
         btm_progress_bar.grid(row=0, column=1)
         btm_text_window.grid(row=1, column=0, columnspan=2)
-
-        # OSX ONLY - bring window to front
-        if platform.system() == MAC:
-            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
-
-    def testing123(event):
-        print("testing 123...")
-        print(event)
